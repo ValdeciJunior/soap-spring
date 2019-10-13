6fb8e884a7578075a68371577ebedeeda74e4d29
Valdeci Rolim <rolim.valdeci@gmail.com>
2019-10-13 09:33:06 -0300

    first commit

---
+++ src/main/java/com/valdev/soap/webservices/customersadministration/CustomersAdministrationApplication.java
@@ -0,0 +1,13 @@
+package com.valdev.soap.webservices.customersadministration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomersAdministrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomersAdministrationApplication.class, args);
+	}
+
+}
